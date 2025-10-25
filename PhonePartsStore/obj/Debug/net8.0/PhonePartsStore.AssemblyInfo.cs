d233ecb8ba6adcfbab73f4a22c061fb17648b8c2
Dez <lenguyentatthang.dev@gmail.com>
2025-10-25 20:08:01 +0700

    change ui

--- PhonePartsStore/obj/Debug/net8.0/PhonePartsStore.AssemblyInfo.cs
+++ PhonePartsStore/obj/Debug/net8.0/PhonePartsStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhonePartsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65dd50abab3f20f98e68104f07cbd2a689cb1714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c9c5501e511cae0a2bbd226997179b498225146")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhonePartsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhonePartsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
